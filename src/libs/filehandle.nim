aaa872251de660e3e526287b21d22c70e407be23
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2022-08-07 17:37:10 +0900

    add validation for graph

--- src/libs/filehandle.nim
+++ src/libs/filehandle.nim
@@ -49,9 +49,30 @@ proc readSettingJson*(filepath: string): GraphOption =
   # read setting file as JSON (for graph).
   var jsonStr: string = parseFile(filepath).pretty
   var obj = parseJson(jsonStr)
+
+  if not obj.hasKey("graph"):
+    echo ERROR & SETTING_FILE_FORMAT_ERROR
+    quit(1)
+
   obj = obj["graph"]
 
-  # TODO: add validation.
+  var errorMessages: seq[string]
+  if not obj.hasKey("colors"):
+    errorMessages.add("colors" & SETTING_FILE_COLUMN_ERROR)
+  if not obj.hasKey("title"):
+    errorMessages.add("title" & SETTING_FILE_COLUMN_ERROR)
+  if not obj.hasKey("size"):
+    errorMessages.add("size" & SETTING_FILE_COLUMN_ERROR)
+  if not obj.hasKey("width"):
+    errorMessages.add("width" & SETTING_FILE_COLUMN_ERROR)
+  if not obj.hasKey("height"):
+    errorMessages.add("height" & SETTING_FILE_COLUMN_ERROR)
+
+  if errorMessages.len > 0:
+    echo ERROR
+    for e in errorMessages:
+      echo e
+    quit(1)
 
   var colors: seq[string]
   for c in obj["colors"].getElems():
